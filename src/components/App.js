e6e04b5a3689b1e3895e8b6955dd6a1f110f12c7
AzaharaGV <azahara1359@gmail.com>
2021-06-06 18:02:24 +0200

    ejercicio2-leccion2

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
 import "./hola.scss";
 import React from "react";
 import MediaCard from "./MediaCard";
+import pic from "../images/foto.png";
 
-function App() {
-  return (
-    <div>
-      <MediaCard />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <MediaCard
+          name="Azahara"
+          date="Hoy igual que ayer, todos los dias lo mismo"
+          image="{pic}"
+          text="Si yo te contara, y tu me escucharas"
+          likes="37"
+          heart="♥"
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/MediaCard.js
+++ src/components/MediaCard.js
@@ -1,23 +1,30 @@
 import pic from "../images/foto.png";
-function MediaCard() {
-  return (
-    <div className="App">
-      <div className="header">
-        <img src={pic} className="picture"></img>
-        <div className="userDate">
-          <h2 className="userName">Azahara</h2>
-          <p className="date">Hoy igual que ayer, todos los dias lo mismo</p>
+import React from "react";
+
+class MediaCard extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="header">
+          <img src={pic} className="picture"></img>
+          <div className="userDate">
+            <h2 className="userName">{this.props.name}</h2>
+            <p className="date">{this.props.date}</p>
+          </div>
+        </div>
+        <p className="textUser">{this.props.text}</p>
+        <div className="footer">
+          <a className="more" href="#"></a>
+          <p className="likes">{this.props.likes}</p>
+          <p className="heart">{this.props.heart}</p>
         </div>
       </div>
-      <p className="textUser"> Si yo te contara, y tu me escucharas</p>
-      <div className="footer">
-        <a className="more" href="#">
-          Más info
-        </a>
-        <p className="likes">37</p>
-        <p className="heart">♥</p>
-      </div>
-    </div>
-  );
+    );
+  }
 }
+
 export default MediaCard;
