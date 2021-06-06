fcf7c3cf8ba35aee2a90478105869e7dcb4c9f8f
AzaharaGV <azahara1359@gmail.com>
2021-06-06 15:46:54 +0200

    Ejercicio 2 leccion uno

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,14 @@
 import "../stylesheets/App.css";
 import pic from "../images/foto.png";
-function App() {
-  return (
-    <div className="App">
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const principal = (
       <div className="header">
         <img src={pic} className="picture"></img>
         <div className="userDate">
@@ -10,8 +16,10 @@ function App() {
           <p className="date">Domingo,5 de junio de 2021</p>
         </div>
       </div>
+    );
+
+    const medium = (
       <p className="textUser">
-        {" "}
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra,
         ipsum vitae consectetur pulvinar, mi lacus tincidunt nisi, non maximus
         mi nunc ut leo. Mauris ut ligula accumsan, pulvinar urna in, venenatis
@@ -21,6 +29,9 @@ function App() {
         et rhoncus dolor fermentum venenatis. Ut ornare, tortor sit amet laoreet
         lobortis, mi diam egestas quam, at pharetra massa massa eu dolor.
       </p>
+    );
+
+    const final = (
       <div className="footer">
         <a className="more" href="#">
           Leer más...
@@ -28,8 +39,15 @@ function App() {
         <p className="likes">37</p>
         <p className="heart">♥</p>
       </div>
-    </div>
-  );
+    );
+
+    return (
+      <div className="App">
+        {principal} {medium}
+        {final}
+      </div>
+    );
+  }
 }
 
 export default App;
