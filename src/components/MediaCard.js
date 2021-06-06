e69f41a4c8a6674855f89a93476f6106584c0a0f
AzaharaGV <azahara1359@gmail.com>
2021-06-06 18:23:22 +0200

    ejercicio3-leccion2

--- src/components/MediaCard.js
+++ src/components/MediaCard.js
@@ -1,30 +1,31 @@
 import pic from "../images/foto.png";
 import React from "react";
 
-class MediaCard extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <div className="header">
-          <img src={pic} className="picture"></img>
-          <div className="userDate">
-            <h2 className="userName">{this.props.name}</h2>
-            <p className="date">{this.props.date}</p>
-          </div>
-        </div>
-        <p className="textUser">{this.props.text}</p>
-        <div className="footer">
-          <a className="more" href="#"></a>
-          <p className="likes">{this.props.likes}</p>
-          <p className="heart">{this.props.heart}</p>
-        </div>
+const MediaCard = (props) => {
+  const principal = (
+    <div className="header">
+      <img src={pic} className="picture"></img>
+      <div className="userDate">
+        <h2 className="userName">{props.name}</h2>
+        <p className="date">{props.date}</p>
       </div>
-    );
-  }
-}
+    </div>
+  );
+  const medium = <p className="textUser">{props.text}</p>;
+  const final = (
+    <div className="footer">
+      <a className="more" href="#"></a>
+      <p className="likes">{props.likes}</p>
+      <p className="heart">{props.heart}</p>
+    </div>
+  );
+  return (
+    <div className="App">
+      {principal}
+      {medium}
+      {final}
+    </div>
+  );
+};
 
 export default MediaCard;
