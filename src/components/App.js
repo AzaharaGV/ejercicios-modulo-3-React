03a3f68864fbeed0d1d536fcba1d49d4f5b6746f
AzaharaGV <azahara1359@gmail.com>
2021-06-06 15:58:30 +0200

    ejercicio3-leccion1

--- src/components/App.js
+++ src/components/App.js
@@ -2,52 +2,38 @@ import "../stylesheets/App.css";
 import pic from "../images/foto.png";
 import React from "react";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const principal = (
+function App() {
+  const data = {
+    image:
+      "https://ucarecdn.com/f8cf81eb-3bab-4bba-9431-668884eab174/-/resize/300x/",
+    cardTitle: "Bob Dylan",
+    cardDate: "May 24, 1941",
+    cardDescription:
+      "Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer/songwriter, author, and artist who has been an influential figure in popular music and culture for more than five decades.",
+    button: {
+      url: "https://en.wikipedia.org/wiki/Bob_Dylan",
+      label: "Go to wikipedia",
+    },
+  };
+  return (
+    <div className="App">
       <div className="header">
-        <img src={pic} className="picture"></img>
+        <img src={data.image} className="picture"></img>
         <div className="userDate">
-          <h2 className="userName">Azahara García</h2>
-          <p className="date">Domingo,5 de junio de 2021</p>
+          <h2 className="userName">{data.cardTitle}</h2>
+          <p className="date">{data.cardDate}</p>
         </div>
       </div>
-    );
-
-    const medium = (
-      <p className="textUser">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra,
-        ipsum vitae consectetur pulvinar, mi lacus tincidunt nisi, non maximus
-        mi nunc ut leo. Mauris ut ligula accumsan, pulvinar urna in, venenatis
-        lorem. Integer auctor, nibh ut semper porttitor, nisi lectus lobortis
-        nibh, eu tincidunt eros risus eu massa. Phasellus lacinia magna vel arcu
-        sodales, at consectetur metus facilisis. Donec imperdiet faucibus justo,
-        et rhoncus dolor fermentum venenatis. Ut ornare, tortor sit amet laoreet
-        lobortis, mi diam egestas quam, at pharetra massa massa eu dolor.
-      </p>
-    );
-
-    const final = (
+      <p className="textUser"> {data.cardDescription}</p>
       <div className="footer">
         <a className="more" href="#">
-          Leer más...
+          {data.button.url}
         </a>
         <p className="likes">37</p>
         <p className="heart">♥</p>
       </div>
-    );
-
-    return (
-      <div className="App">
-        {principal} {medium}
-        {final}
-      </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
