5c4f782e3393f64375da14cb9218343181fd632a
AzaharaGV <azahara1359@gmail.com>
2021-06-07 10:25:11 +0200

    ejercicio1-leccion3

--- src/components/App.js
+++ src/components/App.js
@@ -2,42 +2,37 @@ import React from "react";
 
 class App extends React.Component {
   render() {
-    const items = [
+    const students = [
       {
-        name: "Cereales con chocolate",
-        description: "Cereales rellenos de chocolate",
-        quantity: 2,
-        category: "Cereales",
-        price: 5,
+        promo: "A",
+        name: "Sofía",
+        age: 20,
       },
       {
-        name: "Hamburguesa con queso",
-        description: "Hamburguesa rica y saludable",
-        quantity: 1,
-        category: "Fast-food",
-        price: 15,
+        promo: "B",
+        name: "María",
+        age: 21,
       },
       {
-        name: "Agua mineral",
-        description: "Agua de un charco del Himalaya",
-        quantity: 2,
-        category: "Bebida",
-        price: 5,
+        promo: "A",
+        name: "Lucía",
+        age: 22,
       },
     ];
+    const promofilter = students.filter((promofilter) => {
+      return promofilter.promo === "A";
+    });
     return (
       <div>
         <h1>Pintar listados con React:</h1>
         {/* con este map iteramos el array de items */}
-        {items.map((item, index) => {
+        {promofilter.map((promofilter, index) => {
           // cada return retorna un li
           return (
             <li key={index}>
-              <h2>Nombre: {item.name}</h2>
-              <p>Descripción: {item.description}</p>
-              <p>Cantidad: {item.quantity}</p>
-              <p>Categoría: {item.category}</p>
-              <p>Precio: {item.price}</p>
+              <h2>Nombre: {promofilter.name}</h2>
+              <p>Descripción: {promofilter.age}</p>
+              <p>Promo: {promofilter.promo}</p>
             </li>
           );
           // el map retorna un array de li, es decir, un listado de HTML
